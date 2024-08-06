f695664c2773a6bdaf51e9ac77a96f29cb13af14
yakuphantonintshi <yakupha58756@gmail.com>
2024-08-06 15:31:18 +0200

    API Content

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-// 
+import express from 'express'
+import path from 'path'
+import { connection as db} from './config/index.js'
+
+// Create an express app 
+const app = express()
+const port = +process.env.PORT || 4000
+const router = express.Router()
+
+// Middleware
+app.use(router, express.static('./static'), 
+express.json(), 
+express.urlencoded({
+    extended: true
+}))
+
+// Endpoint
+router.get('^/$|/eShop', (req, res) => {
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+
+// Getting all the users
+router.get('/users', (req, res) => {
+    try {
+        const strQry = `
+        SELECT firstName, lastName, age, emailAdd 
+        FROM Users;
+        `
+        db.query(strQry, (err, results) => {
+            if(err) throw new Error (`Unable to fetch all users`)
+            res.json({
+                status: res.statusCode,
+                results
+            })
+        })
+    }catch (e) {
+        res.json({
+            status: 404,
+            msg: e.message
+        })
+    }
+})
+
+// Getting a single user
+ router.get('/user/:id' , (req,res) => {
+    try{
+        const strQry = `SELECT userID, firstName, lastName, age, emailAdd
+        FROM Users 
+        WHERE userID = ${req.params.id};`
+
+        db.query(strQry, (err, result) => {
+            if (err) throw new Error(' Unable to fetch the user')
+                res.json({
+            status: res.statusCode,
+            result: result[0]
+        })
+        })
+    } catch (e){
+        res.json({
+            status: 404,
+            msg: e.message
+        })
+    }
+ })
+router.get('*', (req, res) => {
+    res.json({
+        status: 404,
+        msg: 'Resource not found'
+    })
+})
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
