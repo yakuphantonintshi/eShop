870b82e4164de0755c9ddd4d7702afe5beecb92d
yakuphantonintshi <yakupha58756@gmail.com>
2024-08-16 14:44:45 +0200

    Card Slots

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="container-fluid">
+<div class="row">
+  <h2 class="display-2"> Welcome to our store</h2>
+</div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
