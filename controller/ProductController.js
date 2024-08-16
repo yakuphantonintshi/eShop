62eb06458c189c730034bcaf8f10f10ce69a4022
yakuphantonintshi <yakupha58756@gmail.com>
2024-08-16 09:53:09 +0200

    Product Controller

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,15 +1,42 @@
-// import express from 'express'
-// import bodyParser from 'body-parser'
-// import { products} from '../model/index.js'
+import express from 'express'
+import { products} from '../model/index.js'
+import { verifyToken} from '../middleware/AuthenticateUser.js'
+import bodyParser from 'body-parser'
 
-// const productRouter = express.Router()
-// productRouter.use(bodyParser.json())
+const productRouter = express.Router()
+productRouter.use(bodyParser.json())
 
-// productRouter.get('/', (req, res) => {
-//     products.fetchProducts(req, res)
-// })
+// getting all products
+productRouter.get('/', verifyToken, (req, res) => {
+    products.fetchProducts(req, res)
+})
 
-// export{
-//     express,
-//     productRouter
-// }
+// recent products
+productRouter.get('/recent', (req, res) => {
+    products.recentProducts(req, res)
+})
+
+// getting one product
+productRouter.get('/:id', verifyToken, (req, res) => {
+    products.fetchProduct(req, res)
+})
+
+// add a new product
+productRouter.post('/add', verifyToken, (req, res) => {
+    products.addProduct(req, res)
+})
+
+// update product
+productRouter.patch('/:id', verifyToken, (req, res) => {
+    products.updateProduct(req, res)
+})
+
+// delete product
+productRouter.delete('/:id', verifyToken, (req, res) => {
+    products.deleteProduct(req, res)
+})
+
+export{
+    express,
+    productRouter
+}
